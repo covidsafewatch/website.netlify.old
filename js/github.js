9d2347920d01630fdb0733b89c2463f5741a0b5c
Olly <ollytheninja@gmail.com>
2020-05-08 10:16:55 +1200

    Add bootstrap, split out css/js

---
+++ js/github.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    $('body').keypress(function (event) {
+        if (event.which == 101) {
+            event.preventDefault();
+            window.location = $('.github-corner')[0].href;
+        }
+    });
+});
